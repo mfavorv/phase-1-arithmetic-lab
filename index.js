91760702a081031cde9437562ea3c1a57bb07d66
Favor Vincent <vfavorm77@gmail.com>
2024-03-19 14:51:26 +0300

    JS arithmetic lab complete

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random;
+random = Math.floor(Math.random())+ 1;
+
+let num3 = 36;
+let num4 = 8;
+let mod  = num3%num4;
+
+let max;
+max = Math.max(9, 15, 20, 7, 13);
